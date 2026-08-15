df32d40dddc88e98a5e55f40cf406359d9317f3c
Parin <parin.theappideas@gmail.com>
2026-08-15 14:24:37 +0530

    Fix Netlify redirect by removing hardcoded local IP index.html

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.aura.ambientradio',
   appName: 'AURA Ambient Radio',
   webDir: 'public',
-  server: {
-    url: 'http://192.168.29.195:3000',
-    cleartext: true,
-    allowNavigation: ['*']
-  }
 };
 
 export default config;
